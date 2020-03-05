1ae757c165f5fc4598d695294a9bf4da1c94e8ec
unknown <2120367@talnet.nl>
2020-03-05 18:20:15 +0100

    'first-commite'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { TestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  names = ['Shailesh', 'Posty', 'Loki', 'Derc', 'BlobBlob', 'Curvy'];
+  unluckyName = 'Hier komt een random naam';
+  oldName;
+
+  constructor() { }
+
+  update() {
+
+    //kies een random naam
+    this.chooseRandomName();
+
+    //verander de naam als hij al gekozen is 
+    this.changeNameIfAlreadyChosen();
+
+  }
+
+  changeNameIfAlreadyChosen() {
+    //als je nieuwe naam overeenkomt met de oude naam kies dan een andere naam
+    while (this.unluckyName == this.oldName) {
+      this.chooseRandomName();
+    }
+    //zet de oude naam als nieuwe naam
+    this.oldName = this.unluckyName;
+  }
+
+  chooseRandomName() {
+    // kies een random getal tussen 0 en de array lengte
+    var randomNumber = Math.floor((Math.random() * this.names.length));
+    // kies de naam van de names array met dat random getal
+    this.unluckyName = this.names[randomNumber];
+  }
 
 }
+
